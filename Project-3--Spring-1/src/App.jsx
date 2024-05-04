fe68fd0dd5ed1e57b5ded1ec81e220b58143dae5
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-05-05 00:26:38 +0200

    fixe some issues

--- Project-3--Spring-1/src/App.jsx
+++ Project-3--Spring-1/src/App.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 import { Footer } from "./components/footer/Footer";
 import { Nav } from "./components/header/Nav";
 import { Card } from "./components/pages/cardDesign/Card";
-import ChildOne from "./components/pages/test-files/ChildOne";
-import ChildTwo from "./components/pages/test-files/ChildTwo";
 
 function App() {
 
 const [getVlaue, setValue]= useState("");
+
+
+
+
 
   return (
     <>

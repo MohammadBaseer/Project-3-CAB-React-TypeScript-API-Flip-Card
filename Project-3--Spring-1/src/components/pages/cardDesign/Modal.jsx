4ff5ea390f0037b74ed376d2091e8bc0e6ae09be
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-05-09 00:00:12 +0200

    Modal close fixed

--- Project-3--Spring-1/src/components/pages/cardDesign/Modal.jsx
+++ Project-3--Spring-1/src/components/pages/cardDesign/Modal.jsx
@@ -1,38 +1,35 @@
-
-function Modal({modalIsOpen, setModalIsOpen, data }) {
-
-   const closeModal = () => {
-    setModalIsOpen(false)
+import React from "react";
+function Modal({ modalIsOpen, setModalIsOpen, data }) {
+  const closeModal = () => {
+    setModalIsOpen(false);
   };
-  // -------------
-  // window.onclick = function(event) {
-  //   var modal = document.getElementById("myModal");
-  //   if (event.target == modal) {
-  //     setModalIsOpen(false)
-  //   }
-    // console.log(modalIsOpen)
-  // };
+
+  const handleModalClick = (event) => {
+    if (event.target.id === "myModal") {
+      setModalIsOpen(false);
+    }
+  };
+
   // ---------------
   return (
     <>
-            <div id="myModal" className="modal model-card" style={modalIsOpen === true ? {display: 'flex'} : {display: 'none'}} >
-
-<div className="model-inner">
-  <div className="model-front">
-    <div className="title">
-      <h1>{data.name}</h1>
-    </div>
-    <img className={"model-image"} src={data.image} alt="Photo" />
-    <div className="model-elment">
-      <p className="species">{data.species}</p>
-      <p className="status">{data.status}</p>
-      <button className="close" onClick={closeModal}>
-        Close
-      </button>
-    </div>
-  </div>
-</div>
-</div>
+      <div id="myModal" className="modal model-card" style={modalIsOpen === true ? { display: "flex" } : { display: "none" }} onClick={handleModalClick} >
+        <div className="model-inner">
+          <div className="model-front">
+            <div className="title">
+              <h1>{data.name}</h1>
+            </div>
+            <img className={"model-image"} src={data.image} alt="Photo" />
+            <div className="model-elment">
+              <p className="species">{data.species}</p>
+              <p className="status">{data.status}</p>
+              <button className="close" onClick={closeModal}>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
